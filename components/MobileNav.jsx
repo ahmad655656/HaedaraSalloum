2c5643266671cd69d53c1f81b6f318bbbbe03537
Haedara Salloum <haedarahasan69@gmail.com>
2026-02-17 20:44:24 +0300

    upodates

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -4,14 +4,7 @@ import { usePathname } from 'next/navigation'
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 import Link from 'next/link';
 import { CiMenuFries } from 'react-icons/ci' 
-
-const links = [
-    { name: "home", path: "/" },
-    { name: "services", path: "/services" },
-    { name: "resume", path: "/resume" },
-    { name: "work", path: "/work" },
-    { name: "contact", path: "/contact" },
-]
+import { links } from '@/constants/navlinks';
 
 const MobileNav = () => {
     const pathname = usePathname();
